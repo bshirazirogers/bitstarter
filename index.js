82bd0d80c2508d5213609419121af6354ec69e22
bshirazirogers <bilal.shirazi@rci.rogers.com>
2015-05-13 01:59:40 +0000

    update

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-	fs.readFileSync('index.html', function (err,data) {
-		if (err) throw err;
+	//fs.readFileSync('index.html', function (err,data) {
+	//	if (err) throw err;
 		//response.send(data.toString());
 		response.send('data.toString()');
 		
-	});
+	//});
 })
 
 app.listen(app.get('port'), function() {
